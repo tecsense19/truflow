981b5b699acb32dd6caa1547e30b6f44cb565ae6
tecsense19 <tecsense19@gmail.com>
2024-03-05 16:04:25 +0530

    lost cart report and select2

--- app/Views/admin/layout/front.php
+++ app/Views/admin/layout/front.php
@@ -27,7 +27,7 @@
         <!-- other css -->
 
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css">
-        
+
         <!-- Page CSS -->
         <!-- Helpers -->
         <script src="<?php echo base_url(); ?>/public/admin/assets/vendor/js/helpers.js"></script>
